67e7263ccd4bb8467a60737df302cdd1599ab1d8
Gardrick <sergey.krambalev@gmail.com>
2024-10-28 12:41:10 +0300

    final

--- pokemons/Watchog.java
+++ pokemons/Watchog.java
@@ -3,7 +3,7 @@ package pokemons;
 import ru.ifmo.se.pokemon.*;
 import moves.status.ThunderWave;
 
-public class Watchog extends Patrat{
+public final class Watchog extends Patrat{
 	public Watchog(String name, int level) {
 		super(name, level);
 		setStats(60, 85, 69, 60, 69, 77);
